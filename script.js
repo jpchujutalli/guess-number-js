e85017a2663029311c8e8a565a4da83db058e55a
JianPierre <jpchujutalli96@gmail.com>
2026-05-07 11:12:56 -0500

    feat: variables, tipos de datos e interaccion con el usuario

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('¡Hola desde JavaScript!')
+let nombre = 'Jian Pierre';
+let edad = 29;
+let estaAprendiendo = true;
+
+console.log(nombre);
+console.log(edad);
+console.log(estaAprendiendo);
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof estaAprendiendo);
+
+const curso = 'code 101';
+const maxIntentos = 10;
+
+console.log(curso);
+console.log(maxIntentos);
+
+
+let nombreUsuario = prompt('¿Como te llamas?');
+let edadUsuario = prompt('¿Cual es tu edad?');
+
+alert('Hola ' + nombreUsuario + ' tienes ' + edadUsuario + ' años');
+
+console.log('Nombre', nombreUsuario);
+console.log('Edad', edadUsuario);
+console.log(typeof edadUsuario);
+
