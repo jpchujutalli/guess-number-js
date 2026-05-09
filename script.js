1f22a650fcf4a2621b8f30103454d80a6c02d91a
JianPierre <jpchujutalli96@gmail.com>
2026-05-09 14:50:22 -0500

    feat: pistas alto/bajo con if/else if/else y segundo intento

--- script.js
+++ script.js
@@ -24,7 +24,45 @@ console.log('Tipo:', typeof intento);
 // Comparar intento con numero secreto 
 
 if (intento === numeroSecreto) {
-    alert('🎉 ¡Correcto! El numero era ' + numeroSecreto);
-} else {
-    alert('❌ ¡Incorrecto! El numero era ' + numeroSecreto);
+    alert('🎉 ¡Correcto! El número era ' + numeroSecreto);
+    console.log('Resultado: ¡GANÓ!');
+}else if (intento > numeroSecreto) {
+    alert('📈 Muy alto. Intenta un número más bajo.');
+    console.log('Resultado: ¡MUY ALTO!');
+}else {
+    alert('📉 Muy bajo. Intenta un número más alto.');
+    console.log('Resultado: ¡MUY BAJO!');
 }
+
+// Segundo intento
+
+if (intento !== numeroSecreto) {
+    let intento2 = prompt('Intenta de nuevo (1-100):');
+    intento2 = Number(intento2);
+
+    if (intento2 === numeroSecreto) {
+        alert('🎉 ¡Correcto! El número era ' + numeroSecreto);
+        console.log('Resultado: ¡GANÓ!');
+    }else if (intento2 > numeroSecreto) {
+        alert('📈 Muy alto. Intenta un número más bajo.');
+        console.log('Resultado: ¡MUY ALTO!');
+    }else {
+        alert('📉 Muy bajo. Intenta un número más alto.');
+        console.log('Resultado: ¡MUY BAJO!');
+    }
+}
+
+
+// AND (&&): ambas deben ser true
+let edad = 25;
+let tieneLicencia = true;
+console.log(edad >= 18 && tieneLicencia); // true
+
+// OR (||): al menos una debe de ser true
+let esFeriado = false;
+let esDomingo = true;
+console.log(esFeriado || esDomingo); // true (Domingo es true)
+
+// NOT (!): invierte el valor
+let lloviendo = false;
+console.log(!lloviendo); // true (NO esta lloviendo)
